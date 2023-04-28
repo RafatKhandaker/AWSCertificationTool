a3256b5738de639339ba139202b2f4eb60344fe0
Red Twizzler <Rafat.Khandaker@gmail.com>
2023-04-28 03:45:59 -0400

    Fix Chapter 3 questions and answers -- update regex

--- src/main/scala/Builder/QABuilder.scala
+++ src/main/scala/Builder/QABuilder.scala
@@ -6,8 +6,8 @@ import Models.{Answer, Options, Question}
 import scala.collection.mutable.ListBuffer
 
 object QABuilder {
-  var p1 = "\\d+\\. ([A-Za-z&-._\\n*(A-Za-z0-9)“”,’—]+( [A-Za-z&-._\\n“”,’(A-Za-z0-9)—]*)+)\\?* (\\n*\\S* *\\n*\\S*)\\n".r
-  var p2 = "[A-D]{1}\\. ([A-Za-z&-._\\/\\n(A-Za-z0-9)*“”,’—;:]+( [A-Za-z&-._“”,’\\/\\n(A-Za-z0-9)—;:]*)+)\\n*".r
+  var p1 = "\\d+\\. ([A-Za-z&-._\\n*$\\/(A-Za-z0-9)“”,’—]+( [A-Za-z&-._\\n$“”,’\\/(A-Za-z0-9)—]*)+)\\?* (\\n*\\S* *\\n*\\S*)\\n".r
+  var p2 = "[A-D]{1}\\. ([A-Za-z&-._\\/\\n$(A-Za-z0-9)*“”,’—;:]+( [A-Za-z&-._“”,’\\/\\n$(A-Za-z0-9)—;:]*)+)\\n*".r
 
   var listQuestions: ListBuffer[Question] = new ListBuffer()
   var listOptions: ListBuffer[Options] = new ListBuffer()
